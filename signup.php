f098b54b0afbe09db8acada7d5c34fd9f556eba5
HOOVER <lh0987936@dt050563.otc.edu>
2025-09-22 10:10:45 -0500

    initial welcome, half initial signup

---
+++ signup.php
@@ -0,0 +1,19 @@
+<?php
+// if 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="signup.php" method="$_POST">
+        <input type="text" name="Name" id="Name">
+        <input type="text" name="FavMovie" id="FavMovie">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,16 @@
+<?php If ($_GET['member'] === false) {
+    header('location: signup.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome Movie Club Member!</h1>
+</body>
+</html>
