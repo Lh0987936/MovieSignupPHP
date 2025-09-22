51ff21af52ac56d1f9cae235ac08c70c6890379a
HOOVER <lh0987936@dt050563.otc.edu>
2025-09-22 09:57:33 -0500

    initial index finished

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Movie Club</h1>
+<h2>Hello! Welcome to the Movie Club! Sign up <a href="signup.php">here</a>!</h2>
+<a href="Welcome.php?member=false">Vip Area</a>
+</body>
+</html>
